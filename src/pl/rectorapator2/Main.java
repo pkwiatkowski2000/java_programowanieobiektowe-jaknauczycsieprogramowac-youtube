b3906fa64bfb2d1e504a34ea98f38bf62f30d947
pkwiatkowski2000 <pawel.kwiatkowski2000@gmail.com>
2023-03-24 13:54:24 +0100

    24.03.2023

--- src/pl/rectorapator2/Main.java
+++ src/pl/rectorapator2/Main.java
@@ -1,4 +1,12 @@
 package pl.rectorapator2;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Monitor monitor = new Monitor();
+        System.out.println(monitor.getResolution());
+
+        monitor.setLowResouliton();
+        System.out.println(monitor.getResolution());
+    }
 }
