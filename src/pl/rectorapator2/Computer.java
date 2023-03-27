7d2f54542ccad6476b672f23aaf70838cc71f1be
pkwiatkowski2000 <pawel.kwiatkowski2000@gmail.com>
2023-03-27 09:26:01 +0200

    27.03.2023

--- src/pl/rectorapator2/Computer.java
+++ src/pl/rectorapator2/Computer.java
@@ -11,7 +11,7 @@ public class Computer {
     private Drive drive;
     private Headphones headphones;
 
-    List<USBDevice> usbDevices = new ArrayList<>();
+   private List<USBDevice> usbDevices = new ArrayList<>();
 
     public Computer(Monitor monitor, Drive drive) {
 
@@ -58,8 +58,10 @@ public class Computer {
     public void removeUSBDevice (USBDevice usbDevice) {
         boolean isDisconnected = usbDevice.disconnect();
 
-        if (isDisconnected) {
-            usbDevices.remove(usbDevice);
+        if (!isDisconnected) {
+            System.out.println("Forcibly removed USBDevice");
         }
+
+        usbDevices.remove(usbDevice);
     }
 }
--- src/pl/rectorapator2/Main.java
+++ src/pl/rectorapator2/Main.java
@@ -2,6 +2,8 @@ package pl.rectorapator2;
 
 import pl.rectorapator2.drive.HDDDrive;
 import pl.rectorapator2.drive.SSDDrive;
+import pl.usbdevice.MemoryStick;
+import pl.usbdevice.Mouse;
 
 public class Main {
 
@@ -12,7 +14,19 @@ public class Main {
 
         Computer computer = new Computer(monitor, drive);
 
-        drive.addFile(new File("Jnsp.jpg"));
-        drive.listFiles();
+//        drive.addFile(new File("Jnsp.jpg"));
+//        drive.listFiles();
+
+        MemoryStick memoryStick = new MemoryStick("Pendrive");
+        Mouse mouse = new Mouse("Mysz");
+
+        computer.addUSBDevice(memoryStick);
+        computer.addUSBDevice(mouse);
+
+        memoryStick.eject();
+        computer.removeUSBDevice(memoryStick);
+        computer.removeUSBDevice(mouse);
+
+        computer.usbDevices
     }
 }
